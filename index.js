38b5c6811a297c181528c1d32f5cdc34e335a40e
Wyatt O'Connell <qdy5bq@virginia.edu>
2023-08-16 14:23:30 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+require('dotenv').config()
+const express = require('express');
+const querystring = require('querystring');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+const port = 8888;
+
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
+const PORT = process.env.PORT || 8888;
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Niche Index");
+});
+
+app.get('/login', (req, res) => {
+
+    const scope = "user-read-private user-read-email playlist-read-private playlist-read-collaborative user-library-read user-top-read";
+
+    const queryParams = querystring.stringify({
+      client_id: CLIENT_ID,
+      response_type: 'code',
+      redirect_uri: REDIRECT_URI,
+      scope: scope,
+    });
+  
+    res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+  
+    axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: querystring.stringify({
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: REDIRECT_URI
+      }),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      },
+    })
+    .then(response => {
+        if (response.status === 200) {
+    
+          const { access_token, refresh_token, expires_in} = response.data;
+    
+          const queryParams = querystring.stringify({
+            access_token,
+            refresh_token,
+            expires_in,
+          });
+  
+          res.redirect(`${FRONTEND_URI}/?${queryParams}`);
+    
+        } else {
+            res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
+        }
+      })
+      .catch(error => {
+        res.send(error);
+      });
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app listening at http://localhost:${PORT}`);
+});
